800fc5a2b4c1baba67f8527a89579a063e95252b
meng <monkeystylle@gmail.com>
2022-07-05 09:41:10 +0800

    added guides content

--- pages/guides.tsx
+++ pages/guides.tsx
@@ -1,10 +1,12 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import styled from 'styled-components';
 import { AuthContext } from '../stores/authContext';
 
 const Guides: NextPage = () => {
   const { user, authReady } = useContext(AuthContext);
+  const [guides, setGuides] = useState<any>(null);
+  const [error, setError] = useState<any>(null);
 
   console.log('user:', user);
   console.log('authReady:', authReady);
@@ -19,12 +21,45 @@ const Guides: NextPage = () => {
           },
         }
       )
-        .then(res => res.json())
-        .then(data => console.log(data));
+        .then(res => {
+          if (!res.ok) {
+            throw Error('You must be logged in to view this content');
+          }
+          return res.json();
+        })
+        .then(data => {
+          setGuides(data);
+          setError(null);
+        })
+        .catch(err => {
+          setError(err.message);
+          setGuides(null);
+        });
     }
   }, [user, authReady]);
 
-  return <GuidesWrapper>guides</GuidesWrapper>;
+  return (
+    <GuidesWrapper>
+      {!authReady && <div>Loading...</div>}
+      {error && (
+        <div>
+          <p>ERROR!!</p>
+        </div>
+      )}
+      {guides &&
+        guides.map(guide => (
+          <div key={guide.title}>
+            <h3>{guide.title}</h3>
+            <h4>Written by {guide.author}</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Asperiores laboriosam, totam illum et quam ex nihil incidunt atque
+              obcaecati perferendis.
+            </p>
+          </div>
+        ))}
+    </GuidesWrapper>
+  );
 };
 
 const GuidesWrapper = styled.div`
